a2fcf94a59699858568bd9e61ff21a669177c6b1
Dheeraj Gupta <dj13042002@gmail.com>
2022-03-09 13:04:51 +0530

    Added login button

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './NavBar.css';
-import thunder from './../../assests/thunder-yellow.png'
+import thunder from './../../assests/thunder-yellow.png';
 
 function NavBar() {
     return (
@@ -8,11 +8,19 @@ function NavBar() {
             <div className="navbar-container">
                 <div className="navbar-left">
                     <img src={thunder} alt="" className="thunder" />
-                    <h1 className='logo-name'>Accelerate</h1>
+                    <h1 className="logo-name">Accelerate</h1>
+                </div>
+                <div className="login-button-container">
+                    <button
+                        className="login-button"
+                        type="button"
+                    >
+                        Login
+                    </button>
                 </div>
             </div>
         </>
-    )
+    );
 }
 
 export default NavBar;
